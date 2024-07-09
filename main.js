3a5b8dec406c7b18eb0007cf762a249fb5a42e27
Daniel Nanovski <nanovsky@gmail.com>
2024-07-09 16:20:32 +0300

    Default percent mode

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 import './style.css'
+
+const updateHash = () => {
+    let sHash = document.location.hash.slice(1);
+
+    // Handle default
+    if (!sHash) {
+        sHash = "37";
+        document.location.hash = sHash;
+    }
+
+    updatePercent(sHash);
+}
+
+const updatePercent = (sPercent) => {
+    document.getElementById("fill").style.width = `${sPercent}%`;
+    document.getElementById("percent").innerHTML = `${sPercent} %`;
+
+    let sFillColor = "#68f79d";
+    if (parseInt(sPercent) <= 20) {
+        sFillColor = "#fbad18";
+    }
+    document.getElementById("fill").style.backgroundColor = sFillColor;
+}
+
+updateHash();
+addEventListener("hashchange", updateHash);
